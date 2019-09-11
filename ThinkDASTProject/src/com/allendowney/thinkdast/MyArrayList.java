1b8b43cdf93eebd6e6f77dc91e78f575ade98eba
evanstpaul16 <evanstpaul16@OlinLab204-09.augustana.edu>
2019-09-11 15:35:02 -0500

    Lab2 TODOs

--- ThinkDASTProject/src/com/allendowney/thinkdast/MyArrayList.java
+++ ThinkDASTProject/src/com/allendowney/thinkdast/MyArrayList.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.ListIterator;
 
 /**
- * @author downey
+ * @author downey & Evan St. Paul
  * @param <T>
  *
  */
@@ -45,7 +45,15 @@ public class MyArrayList<T> implements List<T> {
 	@Override
 	public boolean add(T element) {
 		// TODO: FILL THIS IN!
-		return false;
+		if (size >= array.length) {
+			// make a bigger array and copy over the elements
+			T[] bigger = (T[]) new Object[array.length * 2];
+			System.arraycopy(array, 0, bigger, 0, array.length);
+			array = bigger;
+		}
+		array[size] = element;
+		size++;
+		return true;
 	}
 
 	@Override
@@ -111,6 +119,11 @@ public class MyArrayList<T> implements List<T> {
 	@Override
 	public int indexOf(Object target) {
 		// TODO: FILL THIS IN!
+		for (int i = 0; i < size; i++) {
+			if (equals(target, array[i])) {
+				return i;
+			}
+		}
 		return -1;
 	}
 
@@ -182,7 +195,12 @@ public class MyArrayList<T> implements List<T> {
 	@Override
 	public T remove(int index) {
 		// TODO: FILL THIS IN!
-		return null;
+		T element = get(index);
+		for (int i = index; i < size - 1; i++) {
+			array[i] = array[i + 1];
+		}
+		size--;
+		return element;
 	}
 
 	@Override
@@ -202,7 +220,12 @@ public class MyArrayList<T> implements List<T> {
 	@Override
 	public T set(int index, T element) {
 		// TODO: FILL THIS IN!
-		return null;
+		if (index < 0 || index >= size) {
+			throw new IndexOutOfBoundsException();
+		}
+		T returnVal = array[index];
+		array[index] = element;
+		return returnVal;
 	}
 
 	@Override
